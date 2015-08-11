4a6272179380a9cb22da90e3d807f5063d769a82
kafka <kafka@madrognon.net>
2015-08-11 18:07:08 +0200

    Added a very buggy and stupid word counting function

--- .emacs.d/tools.el
+++ .emacs.d/tools.el
@@ -2,22 +2,45 @@
 
 ;; eshell stuff
 ;; head utility for OSes where it's missing
-(defun eshell/head (filePath n)
-  "Insert the N first lines from the file at filePath."
+(defun eshell/head (filepath n)
+  "Insert the first N lines from the file at FILEPATH."
   (eshell-flush -1)
   (let* ((oldbuf (current-buffer))
 	(contents (with-temp-buffer
-		    (insert-file-contents filePath)
+		    (insert-file-contents filepath)
 		    (split-string (buffer-string) "\n" t))))
     (mapc (lambda (x)
-	   (eshell-buffered-print (concat x "\n"))) (nfirst n contents))
+	   (eshell-buffered-print (concat x "\n"))) (kafka/nfirst n contents))
     (eshell-flush)))
 
-(defun nfirst (n aList)
-  "Returns the N first items from aList."
-  (cond ((> n (length aList)) (nfirst (length aList) aList))
+(defun kafka/nfirst (n elements)
+  "Returns the first N items from ELEMENTS, which must be a list."
+  (cond ((> n (length elements)) (nfirst (length elements) elements))
         ((= n 0) nil)
-        ((eq aList nil) nil)
+        ((eq elements nil) nil)
         ((> n 0)
-           (cons (car aList) (nfirst (1- n) (cdr aList))))))
+           (cons (car elements) (nfirst (1- n) (cdr elements))))))
 
+;; tail utility for OSes where it's missing
+(defun eshell/tail (filepath n)
+  "Insert the last N lines from the file at FILEPATH."
+  (eshell-flush -1)
+  (let* ((oldbuf (current-buffer))
+	(contents (with-temp-buffer
+		    (insert-file-contents filepath)
+		    (split-string (buffer-string) "\n" t))))
+    (mapc (lambda (x)
+	   (eshell-buffered-print (concat x "\n"))) (nthcdr (- (length contents) n) contents))
+    (eshell-flush)))
+
+;; word count
+;; buggy and stupid for now
+(defun kafka/count-words-region (start end)
+  "Count regular words in the region between START and END."
+  (interactive "r")
+    (let ((count 0))
+      (save-excursion
+	(goto-char start)
+	(while (and (< (point) end) (search-forward-regexp "\\sw+"))
+	  (setq count (1+ count))))
+      (message "Number of words in region: %d" count)))
