60d6c9b085512f33747c6d24cd4d9d29d5ab8dd3
kafka <kafka@madrognon.net>
2015-09-05 22:35:17 +0200

    Added basic configuration for gnus (gmail)

---
+++ .emacs.d/gnus.el
@@ -0,0 +1,9 @@
+;; gnus configuration
+
+;; gmail
+
+(require 'gnus)
+(add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
+                                  (nnimap-address "imap.gmail.com")
+                                  (nnimap-server-port 993)
+                                  (nnimap-stream ssl)))
