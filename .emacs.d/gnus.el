5d40599ac02f7b498c6852f7572d2bd7e7a1bc65
kafka <kafka@madrognon.net>
2015-09-06 20:51:38 +0200

    Working mail config for gmail; not there yet for madrognon

--- .emacs.d/gnus.el
+++ .emacs.d/gnus.el
@@ -3,7 +3,14 @@
 ;; gmail
 
 (require 'gnus)
-(add-to-list 'gnus-secondary-select-methods '(nnimap "gmail"
+(setq gnus-select-method '(nnimap "gmail"
                                   (nnimap-address "imap.gmail.com")
-                                  (nnimap-server-port 993)
+                                  (nnimap-server-port "imaps")
                                   (nnimap-stream ssl)))
+
+(add-to-list 'gnus-secondary-select-methods '(nnimap "madrognon"
+						     (nnimap-address "ns300827.ip-91-121-66.eu")
+						     (nnimap-server-port 143)
+						     (nnimap-stream ssl)))
+
+
