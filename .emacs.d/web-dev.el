ffb6e2655c45baac9806a161e267cab37050651d
kafka <kafka@madrognon.net>
2015-09-15 21:23:40 +0200

    Added impatient-mode and simple-httpd for web dev

---
+++ .emacs.d/web-dev.el
@@ -0,0 +1,11 @@
+;; general web development
+;; emacs web server
+(add-to-list 'load-path "~/.emacs.d/floating/emacs-web-server")
+(require 'simple-httpd)
+
+;; impatient-mode
+;; real-time updates in browser
+(add-to-list 'load-path "~/.emacs.d/floating/impatient-mode")
+(require 'impatient-mode)
+
+
