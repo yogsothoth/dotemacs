df593022f55da0dbe02e94a5cd9051ad63fdab26
yogsothoth <kafka@madrognon.net>
2015-09-17 15:45:43 +0200

    Added mysql.el (need to rework it to fit my style)

--- .emacs.d/sql.el
+++ .emacs.d/sql.el
@@ -3,3 +3,7 @@
 	  (lambda ()
 	    (font-lock-add-keywords nil
 				    '(("\\<\\(FIXME\\|TODO\\|BUG\\):" 1 font-lock-warning-face t)))))
+
+
+;; Load RDBMS-specific modes
+(add-to-list 'load-path "~/.emacs.d/floating/rdbms")
