ca312a84f49cfb373ecad4c79cab970a39cbce14
yogsothoth <kafka@madrognon.net>
2015-12-22 18:43:19 +0100

    Fixed PGP/MIME encryption for Sent mail

--- .emacs.d/gnus.el
+++ .emacs.d/gnus.el
@@ -14,5 +14,8 @@
       smtpmail-smtp-service 587
       user-full-name "Nicolas Herry"
       mail-host-address "madrognon.net"
+      mml2015-signers '("16924EF0")
+      mml2015-encrypt-to-self t
       gnus-message-archive-group (format-time-string "nnimap+madrognon:Sent")
-      gnus-gcc-mark-as-read t)
+      gnus-gcc-mark-as-read t
+      user-login-name "kafka")
