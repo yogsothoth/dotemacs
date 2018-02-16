a95cb419b46101b254c90d3aeb64239ad66cc92b
Nicolas Herry <nicolasherry@gmail.com>
2018-02-16 14:48:12 +0100

    Better configuration for slime

--- .emacs.d/lisp.el
+++ .emacs.d/lisp.el
@@ -1,6 +1,9 @@
 ;; lisp.el
 ;; configuration for lisp development
 
-(setq inferior-lisp-program "sbcl")
-(slime-setup '(slime-company))
+(setq inferior-lisp-program "sbcl --dynamic-space-size 2048")
+(add-to-list 'slime-contribs 'slime-autodoc)
+;;(slime-setup '(slime-company))
 (load (expand-file-name "~/quicklisp/slime-helper.el"))
+(load (expand-file-name "~/code/slime-repl-ansi-color/slime-repl-ansi-color.el"))
+(slime-setup '(slime-fancy slime-banner slime-repl-ansi-color))
