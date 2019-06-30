a8ffdcd7c525d7e2a06b5f56413809201e3848f4
Nicolas Herry <beastieboy@beastieboy.net>
2019-06-30 20:53:17 +0200

    Typescript. Clean-up. Ample-theme.

---
+++ .emacs.d/typescript.el
@@ -0,0 +1,14 @@
+;;;; typescript.el
+
+(defun setup-tide-mode ()
+  (interactive)
+  (tide-setup)
+  (flycheck-mode +1)
+  (setq flycheck-check-syntax-automatically '(save mode-enabled))
+  (eldoc-mode +1)
+  (tide-hl-identifier-mode +1)
+  (company-mode +1))
+
+(setq company-tooltip-align-annotations t)
+(add-hook 'before-save-hook 'tide-format-before-save)
+(add-hook 'typescript-mode-hook #'setup-tide-mode)
